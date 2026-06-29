606a49add4776dbaccbb5e69390c84b5a26fec21
n1rmata <kiryl.valiushka@gmail.com>
2026-06-29 16:35:42 +0200

    chore(release): bump adapter-api to 0.67.0 and adapter-core to 0.32.0

--- nx-gs-adapter-core/build.gradle.kts
+++ nx-gs-adapter-core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.shadow)
 }
 
-version = findProperty("${project.name}.version") as String? ?: "0.31.0"
+version = findProperty("${project.name}.version") as String? ?: "0.32.0"
 
 java {
     withSourcesJar()
